222c5d86b718ec6bb6622ec0fb38f7ed68d90bf2
Saviour Assandoh <asandohs@gmail.com>
2023-09-11 01:52:11 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lower_case = 'a';
+
+	while
+		(lower_case <= 'z');
+	{
+		putchar(lower_case);
+		lower_case = lower_case + 1;
+	}
+	return (0);
+}
