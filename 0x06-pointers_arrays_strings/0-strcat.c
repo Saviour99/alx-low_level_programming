430a07595fa529c4bdfff47a95b2f85a38965863
Saviour Assandoh <asandohs@gmail.com>
2023-09-22 13:30:02 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i, j, k;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (j = 0 ; src[j] != '\0' ; j++)
+		srclen++;
+	for (k = 0 ; k <= srclen ; k++)
+		dest[destlen + 1] = src[j];
+	return (dest);
+}
