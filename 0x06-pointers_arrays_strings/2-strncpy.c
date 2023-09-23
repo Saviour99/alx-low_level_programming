56618c31a1f782de685ada770f9c7c3e65184ba5
Saviour Assandoh <asandohs@gmail.com>
2023-09-22 18:16:01 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strncpy - copy two strings
+ *@dest: string 1
+ *@src: string 2
+ *@n: characters to copy
+ *Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	for (i = 0 ; i < n ; i++)
+		dest[i] = '\0';
+	return (dest);
+}
