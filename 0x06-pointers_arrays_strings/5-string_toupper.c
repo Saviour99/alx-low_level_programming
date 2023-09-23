4ba50e1074ddf5c6f3e183d1d8cd0b9867ab770e
Saviour Assandoh <asandohs@gmail.com>
2023-09-22 19:08:55 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *string_toupper - lowercase to uppercase
+ *@n: pointer
+ *Return: char
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0 ; n[i] != '\0' ; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
