cb2f25b168beb0cbcc48d0a527a9e943787063a5
Saviour Assandoh <asandohs@gmail.com>
2023-09-29 22:48:31 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - print the name of a program
+ *@argc: argument count
+ *@argv: argument array
+ *Return: name
+ */
+
+int main(int argc, char *argv[])
+{
+	 printf("%s\n", argv[0]);
+	 return (0);
+}
