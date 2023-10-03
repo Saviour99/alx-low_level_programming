fddc7598ac4e773de95ab440102b441042420157
Saviour Assandoh <asandohs@gmail.com>
2023-10-02 22:15:30 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,14 +16,16 @@ char *str_concat(char *s1, char *s2)
 	int len2 = 0;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		len1++;
 	for (i = 0 ; s2[i] != '\0' ; i++)
 		len2++;
+
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0 ; s1[i] != '\0' ; i++)
