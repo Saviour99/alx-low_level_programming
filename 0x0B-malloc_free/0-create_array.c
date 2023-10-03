5c3b96fec15be6534a5940707ccce7993c74033a
Saviour Assandoh <asandohs@gmail.com>
2023-10-02 18:42:41 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,17 +13,12 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	i = 0;
-	while (i < size)
-		i++;
 	if (size == 0)
-	{
 		return (NULL);
-	}
 	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0 ; i < size ; i++)
-	{
 		ptr[i] = c;
-	}
 	return (ptr);
 }
