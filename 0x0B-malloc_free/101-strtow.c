49676b49a002cd3f699958103c1745cc65237fe8
Saviour Assandoh <asandohs@gmail.com>
2023-10-03 21:27:24 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 
 /**
+ *count_word - count numbers
+ *@s: string
+ *Return: words
+ */
+
+int count_word(char *s)
+{
+	int a, b, c;
+
+	a = 0;
+	c = 0;
+
+	for (b = 0; s[b] != '\0' ; b++)
+	{
+		if (s[b] == ' ')
+			a = 0;
+		else if (a == 0)
+		{
+			a = 1;
+			c++;
+		}
+	}
+	return (c);
+}
+
+
+
+/**
  *strtow - split a string into words
  *@str: string
  *
@@ -10,34 +38,36 @@
 
 char **strtow(char *str)
 {
-	char **ptr;
-	int i = 0, words, w, lets, l;
+	char **ptr, word;
+	int i, j = 0, len = 0, let, x = 0, start, end;
 
-	if (ptr == NULL || ptr[0] == '\0')
+	while (*(str + len))
+		len++;
+	let = count_word(str);
+	if (let == 0)
 		return (NULL);
-	if (words == 0)
-		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
 
-	if (ptr == NULL)
-		return (NULL);
-	for (w = 0 ; w < words ; w++)
+	for (i = 0 ; i <= len ; i++)
 	{
-		while (ptr[i] == ' ')
-			i++;
-		ptr[w] = malloc(sizeof(char) * (lets + 1));
-
-		if (ptr[w] == NULL)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			for (; w >= 0 ; w--)
-				free(ptr[w]);
-			free(ptr);
-			return (NULL);
+			if (x)
+			{
+				end = i;
+				ptr = malloc(sizeof(char) * (x + 1));
+				if (ptr == NULL)
+					return (NULL);
+				while (start < end)
+					**ptr++ = str[start++];
+				*ptr = '\0';
+				word[j] = ptr - x;
+				j++;
+				x = 0;
+			}
 		}
-		for (l = 0 ; l < lets ; l++)
-			ptr[w][l] = str[i++];
-		ptr[w][l] = '\0';
+		else if (x++ == 0)
+			start = i;
 	}
-	ptr[w] = NULL;
-	return (ptr);
+	word[j] == NULL;
+	return (word);
 }
