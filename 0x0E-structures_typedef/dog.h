dff8942a223970664ebe54c6580beb4f85edcf48
Saviour Assandoh <asandohs@gmail.com>
2023-10-10 12:49:41 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - atructure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: struct
+ */
+
 	struct dog
 {
 	char *name;
