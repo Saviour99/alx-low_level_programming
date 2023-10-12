75dd204f4a30ff2f2e15ff644fe232ac2dee3ced
Saviour Assandoh <asandohs@gmail.com>
2023-10-11 17:43:49 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *print_name - Entry point
+ *@name: string
+ *@f: function pointer
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+		return;
+	(*f)(name);
+}
