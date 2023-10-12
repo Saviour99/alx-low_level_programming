60f76548227abac5bfc9e6cea30f2b118f0cb448
Saviour Assandoh <asandohs@gmail.com>
2023-10-11 18:41:01 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ *op_add - add parameters
+ *@a: num1
+ *@b: num2
+ *Return: integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - subtract parameters
+ *@a: num1
+ *@b: num2
+ *Return: integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiply parameters
+ *@a: num1
+ *@b: num2
+ *Return: integer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divide parameters
+ *@a: num1
+ *@b: num2
+ *Return: integer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - modulo
+ *@a: num1
+ *@b: num2
+ *Return: integer
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
