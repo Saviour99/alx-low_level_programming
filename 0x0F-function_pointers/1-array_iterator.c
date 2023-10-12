ad0436b24f9623d1dc86a64abe0d57770bfc2dea
Saviour Assandoh <asandohs@gmail.com>
2023-10-11 18:03:01 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *array_iterator - execute a function
+ *@array:array of integers
+ *@size: sizeof of size
+ *@action: function pointer
+ *
+ *Return: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
