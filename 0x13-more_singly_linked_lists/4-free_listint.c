fcceed6a2b137a9769ee943fac2ff89e5cb021b1
Saviour Assandoh <asandohs@gmail.com>
2023-10-24 12:17:04 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ *free_listint - frees a list
+ *@head: head of the node
+ *
+ *Return: 0
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(temp);
+		free(head);
+	}
+}
