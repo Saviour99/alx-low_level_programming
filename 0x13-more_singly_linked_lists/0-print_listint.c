997e894a96f7f6ffc466710e1f13030876ddcb17
Saviour Assandoh <asandohs@gmail.com>
2023-10-24 08:06:52 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *print_listint - print all node data
+ *@h: head
+ *
+ *Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	int count = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		count++;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return (count);
+}
