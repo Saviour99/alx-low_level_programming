e30a4085d6556a3cbd3fb6d7709c02c97ed666c0
Saviour Assandoh <asandohs@gmail.com>
2023-10-26 21:17:42 -0700

    task 0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,6 +12,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i;
 	unsigned int result = 0;
 
+	if (!b)
+		return (0);
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
