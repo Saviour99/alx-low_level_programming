276002729a3d73a3b44c7e888b493e6ea261492f
Saviour Assandoh <asandohs@gmail.com>
2023-11-03 21:10:33 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,13 +2,35 @@
 
 /**
  *create_file - function that create a file
- *@filename: file pointer variable
- *@text_content: string
+ *@filename: name of the file
+ *@text_content:string
  *
  * Return: 0
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	
+	FILE *fptr;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fptr = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+
+	if (fptr == NULL)
+		return (-1);
+	if (text_content)
+	{
+		ssize_t written_bytes = write(fptr, text_content, strlen(text_content));
+		if (written_bytes == -1)
+		{
+			close(fptr);
+			return (-1);
+		}
+	}
+
+	close(fptr);
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcnt1.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
