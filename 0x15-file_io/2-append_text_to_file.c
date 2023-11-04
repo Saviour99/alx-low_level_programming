21a22e1d291c545b3ff65b90b87db2e85b148e7c
Saviour Assandoh <asandohs@gmail.com>
2023-11-03 23:02:09 -0700

    TASK 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, 0_WRONLY | 0_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == NULL)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
