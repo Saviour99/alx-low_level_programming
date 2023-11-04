5e44171b648266ce2a8d9912354c7a530f1b0129
Saviour Assandoh <asandohs@gmail.com>
2023-11-03 22:40:06 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <fcnt1.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
