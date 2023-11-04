4fa38e80cd3890b26f9f52b7396a81b05f0a09e8
Saviour Assandoh <asandohs@gmail.com>
2023-11-03 21:55:41 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 /**
  *append_text_to_file - append text at the end of a file
- *@filename: file pointer variable
+ *@filename: name of the file
  *@text_content: string
  *
  * Return: 0
@@ -10,5 +10,29 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		text_content = "";
+		return (-1);
+	}
+
+	fd = open(filename, 0_WRONLY | 0_APPEND);
+
+	if (fd == NULL)
+		return (-1);
+
+	if (text_content)
+	{
+		ssize_t permission = write(fd, text_content, strlen(text_content));
+
+		if (permission == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
+
