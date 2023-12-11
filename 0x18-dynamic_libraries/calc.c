6489a3862648887988a4d38ed3ed37465fdf8fd5
Saviour Assandoh <asandohs@gmail.com>
2023-12-11 14:04:32 -0800

    task 2

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ *add - add two numbers
+ *@a: first number
+ *@b: second number
+ *
+ * Return: addition
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ *sub - subtract two numbers
+ *@a: first number
+ *@b: second number
+ *
+ * Return: subtration
+ */
+
+int sub(int a, int b)
+{
+        return a - b;
+}
+
+/**
+ *mul - multiply two numbers
+ *@a: first number
+ *@b: second number
+ *
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+        return a * b;
+}
+
+/**
+ *div - divides two numbers
+ *@a: first number
+ *@b: second number
+ *
+ * Return: division
+ */
+
+int div(int a, int b)
+{
+        return a / b;
+}
+
+/**
+ *mod - moduloof two numbers
+ *@a: first number
+ *@b: second number
+ *
+ * Return: modulo
+ */
+
+int mod(int a, int b)
+{
+        return a % b;
+}
