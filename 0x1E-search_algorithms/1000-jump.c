981d0d6f08c07983b0f0db9f830c700584d4aa69
Saviour Assandoh <asandohs@gmail.com>
2024-07-16 18:33:22 -0700

    advance 100

--- 0x1E-search_algorithms/1000-jump.c
+++ 0x1E-search_algorithms/1000-jump.c
@@ -9,18 +9,20 @@
  *
  * Return: The first index where the value is located, or -1 if not found
  */
+
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, start = 0, end = 0, step = (size_t)sqrt(size);
+	size_t i, start = 0, end = 0, steps = sqrt(size);
 
 	if (array == NULL)
 		return (-1);
 
-	while (end < size && array[end] < value)
+	while (array[end] < value && end < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", end, array[end]);
+
 		start = end;
-		end += step;
+		end += steps;
 	}
 
 	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
@@ -28,10 +30,10 @@ int jump_search(int *array, size_t size, int value)
 	for (i = start; i <= end && i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
 		if (array[i] == value)
 			return (i);
 	}
 
 	return (-1);
 }
-
